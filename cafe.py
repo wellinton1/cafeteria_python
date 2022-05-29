833382cf0492bee9a79532d49f2937b9db2cf734
wellinton1 <skyred1@gmail.com>
2022-05-29 20:06:55 -0300

    ..

---
+++ cafe.py
@@ -0,0 +1,7 @@
+cafe = str(input('ola,Seu cafe esta quente gostaria de esfriar: '))
+nao  = 0
+sim = 1
+if cafe == 'sim' :
+    print('seu cafe esta esfriando')
+else:
+    print('seu cafe continua quente')
