a310343dba32c358725d2c327f82deb5e5305aee
wellinton1 <skyred1@gmail.com>
2022-05-29 11:11:37 -0300

    adicionado resolução Download

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -1,15 +1,17 @@
 from pytube import YouTube
 
-link =input("cole seu link do youtube para Download: ")
+link = input("Cole seu link do youtube para Download: ")
+video = input("Coloque a resolução ex: 144p,240p,360p,720p...: ")
 try:
     yt = YouTube(link)
+    video = yt.get()
 except:
-    print("erro de conexao")
-
+    pass
 
 try:
-    yt.streams.filter(progressive=True, file_extension="mp4").get_highest_resolution().download()
-    #yt.streams.filter(progressive=True, file_extension="mp4").first().download() biblioteca pytube
+    yt.streams.filter.(res=video).first().download()
+    #yt.streams.filter(progressive=True, file_extension="mp4").get_highest_resolution().download()
+    #yt.streams.filter(progressive=True, file_extension="mp4").first().download() # biblioteca pytube
 except:
     print("erro Download")
-print("donload completo")
+print("Download completo")
