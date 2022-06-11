fd732f75ce3af068064a13f42a9650f0dbb7d75a
wellinton1 <skyred1@gmail.com>
2022-06-11 16:07:46 -0300

    pull

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -1,14 +1,16 @@
+from pytube.cli import on_progress
 from pytube import YouTube
 
 link = input("Cole seu link do youtube para Download: ")
-video = input("Coloque a resolução ex: 144p,240p,360p,720p...: ")
+video = input("Coloque a resolução ex: 144p, 240p ,360p ,480p ,720p...: ")
 try:
-    yt = YouTube(link)
+    yt = YouTube(link, on_progress_callback=on_progress)
     video = yt.get()
 except:
     pass
 try:
     yt.streams.filter(res=video).first().download()
 except:
-    print("erro Download")
-print("Download completo")
+    print("donwload erro")
+else:
+    print("Download completo : ")
