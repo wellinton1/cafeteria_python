237116501f5ede4001b5c64c3be6148e3359db88
wellinton1 <skyred1@gmail.com>
2022-06-14 23:29:20 -0300

    Delete cafe.py

--- cafe.py
+++
@@ -1,7 +0,0 @@
-cafe = str(input('ola,Seu cafe esta quente gostaria de esfriar: '))
-nao  = 0
-sim = 1
-if cafe == 'sim' :
-    print('seu cafe esta esfriando')
-else:
-    print('seu cafe continua quente')
